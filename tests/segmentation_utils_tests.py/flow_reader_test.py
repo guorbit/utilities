c4dc9dd3cf83c1ee1800dd208da4891ca6aa957c
Sajtospoga01 <abodrogai@gmail.com>
2023-03-15 14:58:06 +0000

    abstract lambda mock functions and generator arguments to variable for reusability

--- tests/segmentation_utils_tests.py/flow_reader_test.py
+++ tests/segmentation_utils_tests.py/flow_reader_test.py
@@ -15,6 +15,21 @@ def flow_from_directory_mock(*args, **kwargs):
     return batch
 
 
+generator_args = {
+    "image_path": "tests/segmentation_utils_tests/flow_reader_test",
+    "mask_path": "tests/segmentation_utils_tests/flow_reader_test",
+    "image_size": (512, 512),
+    "output_size": (256 * 256, 1),
+    "num_classes": 7,
+    "shuffle": True,
+    "batch_size": 2,
+    "seed": 909,
+}
+
+mock_onehot_fn = lambda x, y, z: np.rollaxis(np.array([x for i in range(z)]), 0, 3)
+mock_augmentation_fn = lambda x, y, z, a, b: (x, y)
+
+
 def test_makes_flow_generator() -> None:
     patch = MonkeyPatch()
     # mock an imagedatagenerator from keras
@@ -26,16 +41,7 @@ def test_makes_flow_generator() -> None:
     patch.setattr(FlowGenerator, "preprocess", lambda self, x: x)
 
     # create a flow generator
-    flow_generator = FlowGenerator(
-        image_path="tests/segmentation_utils_tests/flow_reader_test",
-        mask_path="tests/segmentation_utils_tests/flow_reader_test",
-        image_size=(512, 512),
-        output_size=(256*256,1),
-        num_classes=7,
-        shuffle=True,
-        batch_size=2,
-        seed=909,
-    )
+    flow_generator = FlowGenerator(**generator_args)
     pass
 
 
@@ -48,25 +54,15 @@ def test_flow_generator_with_preprocess() -> None:
         flow_from_directory_mock,
     )
     # mock external dependencies
-    patch.setattr(
-        ImagePreprocessor, "augmentation_pipeline", lambda x, y, z, a, b: (x, y)
-    )
+    patch.setattr(ImagePreprocessor, "augmentation_pipeline", mock_augmentation_fn)
     patch.setattr(
         ImagePreprocessor,
         "onehot_encode",
-        lambda x, y, z: np.rollaxis(np.array([x for i in range(z)]), 0, 3),
+        mock_onehot_fn,
     )
+
     # create a flow generator
-    flow_generator = FlowGenerator(
-        image_path="tests/segmentation_utils_tests/flow_reader_test",
-        mask_path="tests/segmentation_utils_tests/flow_reader_test",
-        image_size=(512, 512),
-        output_size=(256*256,1),
-        num_classes=7,
-        shuffle=True,
-        batch_size=2,
-        seed=909,
-    )
+    flow_generator = FlowGenerator(**generator_args)
     patch.undo()
     patch.undo()
 
@@ -74,58 +70,39 @@ def test_flow_generator_with_preprocess() -> None:
 def test_get_dataset_size() -> None:
     patch = MonkeyPatch()
     patch.setattr(os, "listdir", lambda x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        # mock an imagedatagenerator from keras
+    # mock an imagedatagenerator from keras
     mock_image_datagen = patch.setattr(
         ImageDataGenerator,
         "flow_from_directory",
         flow_from_directory_mock,
     )
     # mock external dependencies
-    patch.setattr(
-        ImagePreprocessor, "augmentation_pipeline", lambda x, y, z, a, b: (x, y)
-    )
+    patch.setattr(ImagePreprocessor, "augmentation_pipeline", mock_augmentation_fn)
     patch.setattr(
         ImagePreprocessor,
         "onehot_encode",
-        lambda x, y, z: np.rollaxis(np.array([x for i in range(z)]), 0, 3),
+        mock_onehot_fn,
     )
     # create a flow generator
-    flow_generator = FlowGenerator(
-        image_path="tests/segmentation_utils_tests/flow_reader_test",
-        mask_path="tests/segmentation_utils_tests/flow_reader_test",
-        image_size=(512, 512),
-        output_size=(256*256,1),
-        num_classes=7,
-        shuffle=True,
-        batch_size=2,
-    )
+    flow_generator = FlowGenerator(**generator_args)
     size = flow_generator.get_dataset_size()
     assert size == 10
     patch.undo()
     patch.undo()
     patch.undo()
 
+
 def test_get_generator() -> None:
     patch = MonkeyPatch()
     # mock external dependencies
-    patch.setattr(
-        ImagePreprocessor, "augmentation_pipeline", lambda x, y, z, a, b: (x, y)
-    )
+    patch.setattr(ImagePreprocessor, "augmentation_pipeline", mock_augmentation_fn)
     patch.setattr(
         ImagePreprocessor,
         "onehot_encode",
-        lambda x, y, z: np.rollaxis(np.array([x for i in range(z)]), 0, 3),
+        mock_onehot_fn,
     )
     # create a flow generator
-    flow_generator = FlowGenerator(
-        image_path="tests/segmentation_utils_tests/flow_reader_test",
-        mask_path="tests/segmentation_utils_tests/flow_reader_test",
-        image_size=(512, 512),
-        output_size=(256*256,1),
-        num_classes=7,
-        shuffle=True,
-        batch_size=2,
-    )
+    flow_generator = FlowGenerator(**generator_args)
     generator = flow_generator.get_generator()
     assert generator
     patch.undo()
