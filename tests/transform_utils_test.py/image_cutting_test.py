50d09d6346f4ee549944aae7b7231f2909295107
Sajtospoga01 <abodrogai@gmail.com>
2023-03-22 13:17:40 +0000

    Adds error handing tests for image cutter

--- tests/transform_utils_test.py/image_cutting_test.py
+++ tests/transform_utils_test.py/image_cutting_test.py
@@ -1,24 +1,64 @@
 import numpy as np
-from utilities.transform_utils.image_cutting import image_cut, image_stich, image_cut_experimental
-
+from utilities.transform_utils.image_cutting import (
+    image_cut,
+    image_stich,
+    image_cut_experimental,
+)
+import pytest
 
 
 def test_image_cut() -> None:
     img = np.zeros((512, 512, 3))
-    img[-1,-1,0] = 1
-    cut_ims = image_cut(img, (256, 256), num_bands = 3)
+    img[-1, -1, 0] = 1
+    cut_ims = image_cut(img, (256, 256), num_bands=3)
 
     assert cut_ims.shape == (4, 256, 256, 3)
-    assert cut_ims[0, 0, 0, 0] == 0   
+    assert cut_ims[0, 0, 0, 0] == 0
     assert cut_ims[-1, -1, -1, 0] == 1
+
+
+@pytest.mark.xfail
+def test_image_cut_incorrect_shape_colum_vector() -> None:
+    # does not pass
+    try:
+        img = np.zeros((512))
+        img[-1, -1, 0] = 1
+        image_cut(img, (256, 256), num_bands=3)
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_image_cut_incorrect_shape_too_many() -> None:
+    # does not pass
+    try:
+        img = np.zeros((512, 512, 3, 3))
+        img[-1, -1, 0] = 1
+        image_cut(img, (256, 256), num_bands=3)
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_image_cut_incorrect_band_specified() -> None:
+    # test doesnt work, it passes even though it should fail
+    try:
+        img = np.zeros((512, 512, 5))
+        img[-1, -1, 0] = 1
+        image_cut(img, (256, 256), num_bands=3)
+        assert False
+    except ValueError as e:
+        print(e)
+        assert True
+
 
 def test_image_cut_slack_cut() -> None:
     img = np.zeros((513, 513, 3))
-    img[-2,-2,0] = 1
-    cut_ims = image_cut_experimental(img, (256, 256), num_bands = 3, pad = False)
+    img[-2, -2, 0] = 1
+    cut_ims = image_cut_experimental(img, (256, 256), num_bands=3, pad=False)
 
     assert cut_ims.shape == (4, 256, 256, 3)
-    assert cut_ims[0, 0, 0, 0] == 0   
+    assert cut_ims[0, 0, 0, 0] == 0
     assert cut_ims[-1, -1, -1, 0] == 1
 
 
@@ -34,9 +74,8 @@ def test_image_stich() -> None:
         img3[:, :, i] = 6 + i
         img4[:, :, i] = 9 + i
 
-    stiched_img = image_stich(np.array([img1, img2, img3, img4]), 2,2 , (256,256))
+    stiched_img = image_stich(np.array([img1, img2, img3, img4]), 2, 2, (256, 256))
 
-    
     assert stiched_img.shape == (512, 512, 3)
     assert stiched_img[0, 0, 0] == 0
     assert stiched_img[-1, -1, 0] == 9
