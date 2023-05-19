1da4580f1bb92b3be1a16e30d2497b13023cb3c9
Andras Bodrogai <abodrogai@gmail.com>
2023-05-19 21:11:48 +0100

    fixes docs in image stats, updates image stats to able to export doc frequency

--- utilities/transform_utils/image_stats.py
+++ utilities/transform_utils/image_stats.py
@@ -5,39 +5,48 @@ from PIL import Image
 from tqdm import tqdm
 
 
-def get_distribution_seg(path: str, files: list[str], num_classes: int = 7):
+def get_distribution_seg(path, files: list[str], num_classes: int = 7):
     """
-    Returns the distribution of classes in the segmentation dataset,
-    and the number of files the class is present in.
+    Returns the distribution of classes in the segmentation dataset
 
-    Parameters
-    ----------
-    :param path: path to the directory containing the segmentation masks
-    :param files: list of files in the directory
+    Arguments
+    ---------
+    :str path: path to the folder containing the segmentation masks
+    :list[str] files: list of files in the folder
 
     Keyword Arguments
     -----------------
-    :param num_classes: number of classes in the dataset
+    :int, optional num_classes: number of classes in the dataset, defaults to 7
 
     Returns
     -------
-    :return tupple(dict,dict): dictionary containing the distribution of classes
+    :tupple(dict, dict): a tuple containing the distribution of classes and the number of files containing each class
     """
+
     set_of_files = set(files)
     distribution = {}
+    df = {}
     for i in range(num_classes):
         distribution[i] = 0
+        df[i] = 0
 
     for file in tqdm(set_of_files):
         img = Image.open(os.path.join(path, file))
+        size = img.size
+        max = size[0] * size[1]
         img = np.array(img)
         for i in range(num_classes):
-            distribution[i] += np.sum(img == i)
-    return distribution
+            sum = np.sum(img == i)
+            distribution[i] += sum / max
+            if sum > 0:
+                df[i] += 1
+
+    return distribution, df
+
 
 
 if __name__ == "__main__":
-    mask_path = "cut_masks"
-    mask_files = os.listdir(mask_path)
-    dist = get_distribution_seg(mask_path, mask_files)
+    path = "cut_masks"
+    files = os.listdir(path)
+    dist = get_distribution_seg(path, files)
     print(dist)
