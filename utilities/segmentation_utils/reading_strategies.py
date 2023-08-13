e062bf9d1460272a3a7714a17483cad79bae1cfd
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-08-13 12:14:36 +0100

    changes to interface name, get function and constructor

--- utilities/segmentation_utils/reading_strategies.py
+++ utilities/segmentation_utils/reading_strategies.py
@@ -8,7 +8,7 @@ from flowreader import FlowGeneratorExperimental
 from utilities.segmentation_utils import ImagePreprocessor
 
 
-class ReaderInterface(Protocol):
+class IReader(Protocol):
 
     def read_batch(self, start:int, end: int) -> None:
         ...
@@ -22,25 +22,25 @@ class RGB_Image_Strategy:
         self,
         image_path: str,
         image_size: tuple [int, int],
-        batch_image_filenames: np.ndarray,
     ):
         self.image_path = image_path
         self.image_size = image_size
-        self.batch_image_files = batch_image_filenames
 
     def read_batch(self, batch_size, dataset_index) -> np.ndarray:
         #read images with PIL
+        image_filenames = np.array(sorted(os.listdir(self.image_path)))
         for i in range(batch_size):
             image_index = i + dataset_index
             image = Image.open(
-                os.path.join(self.image_path, self.batch_image_filenames[image_index])
+                os.path.join(self.image_path, image_filenames[image_index])
             ).resize(self.image_size, Image.ANTIALIAS)
             image = np.array(image)
             image = image / 255
         return image
 
-    def get_dataset_size(self) -> int:
-        dataset_size = FlowGeneratorExperimental.__len__
+    def get_dataset_size(self, mini_batch) -> int:
+        image_filenames = np.array(sorted(os.listdir(self.image_path)))
+        dataset_size = int(np.floor(len(image_filenames) / float(mini_batch)))
         return dataset_size
 
 class Mask_Image_Strategy:
@@ -48,23 +48,23 @@ class Mask_Image_Strategy:
     def __init__(
         self,
         mask_path: str,
-        batch_mask_filenames: np.ndarray,
         output_reshape: tuple[int, int],
     ):
         self.mask_path = mask_path
-        self.batch_mask_filenames = batch_mask_filenames
         self.output_reshape = output_reshape
     
     def read_batch(self, batch_size, dataset_index) -> np.ndarray:
         #read images with PIL
+        mask_filenames = np.array(sorted(os.listdir(self.mask_path)))
         for i in range(batch_size):
             image_index = i + dataset_index
             mask = Image.open(
-                os.path.join(self.mask_path, self.batch_mask_filenames[image_index])
+                os.path.join(self.mask_path, mask_filenames[image_index])
                 ).resize(self.output_reshape)
             mask = np.array(mask)
         return mask
 
-    def get_dataset_size(self) -> int:
-        dataset_size = FlowGeneratorExperimental.__len__
+    def get_dataset_size(self, mini_batch) -> int:
+        image_filenames = np.array(sorted(os.listdir(self.image_path)))
+        dataset_size = int(np.floor(len(image_filenames) / float(mini_batch)))
         return dataset_size
