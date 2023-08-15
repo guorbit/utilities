7f6798b0dac41d17c9ff6066dc3cd87393c6daee
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-08-15 18:16:48 +0100

    added strategies using rasterio to read images

--- utilities/segmentation_utils/reading_strategies.py
+++ utilities/segmentation_utils/reading_strategies.py
@@ -4,10 +4,6 @@ import numpy as np
 from PIL import Image
 import rasterio
 
-from flowreader import FlowGeneratorExperimental
-from utilities.segmentation_utils import ImagePreprocessor
-
-
 class IReader(Protocol):
 
     def read_batch(self, start:int, end: int) -> None:
@@ -65,6 +61,59 @@ class Mask_Image_Strategy:
         return mask
 
     def get_dataset_size(self, mini_batch) -> int:
+        mask_filenames = np.array(sorted(os.listdir(self.mask_path)))
+        dataset_size = int(np.floor(len(mask_filenames) / float(mini_batch)))
+        return dataset_size
+    
+class Hyperspectral_Image_Strategy:
+
+    def __init__(
+        self,
+        image_path:str,
+    ):
+        self.image_path = image_path
+  
+    def read_batch(self, batch_size, dataset_index) -> np.ndarray:
+        #read images with rasterio
+        image_filenames = np.array(sorted(os.listdir(self.image_path)))
+        for i in range(batch_size):
+            image_index = i + dataset_index
+            #open the source raster dataset
+            with rasterio.open(
+                os.path.join(self.image_path, image_filenames[image_index])
+                ) as dataset:
+                 #.read() returns a numpy array that contains the raster cell values in your file.
+                image = dataset.read()
+                image = image / 255
+        return image
+    
+    def get_dataset_size(self, mini_batch) -> int:
         image_filenames = np.array(sorted(os.listdir(self.image_path)))
         dataset_size = int(np.floor(len(image_filenames) / float(mini_batch)))
+        return dataset_size
+
+class Hyperspectral_Mask_Image_Strategy:
+
+    def __init__(
+        self,
+        mask_path:str,
+    ):
+        self.mask_path = mask_path
+
+    def read_batch(self, batch_size, dataset_index) -> np.ndarray:
+        #read images with rasterio
+        mask_filenames = np.array(sorted(os.listdir(self.mask_path)))
+        for i in range(batch_size):
+            image_index = i + dataset_index
+            #open the source raster dataset
+            with rasterio.open(
+                os.path.join(self.mask_path, mask_filenames[image_index])
+                ) as dataset:
+                #.read() returns a numpy array that contains the raster cell values in your file.
+                mask = dataset.read()
+        return mask
+    
+    def get_dataset_size(self, mini_batch) -> int:
+        mask_filenames = np.array(sorted(os.listdir(self.mask_path)))
+        dataset_size = int(np.floor(len(mask_filenames) / float(mini_batch)))
         return dataset_size
