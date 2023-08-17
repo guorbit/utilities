cd9cd37659e0b2a6faa8555b425563bac9b5eba4
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-08-17 13:43:01 +0100

    added tests for strategies - they do not yet pass

---
+++ tests/segmentation_utils_tests.py/test_strategies.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+from PIL import Image
+from pytest import MonkeyPatch
+from utilities.segmentation_utils.reading_strategies import RGBImageStrategy
+
+def test_read_batch_image_path() -> None:
+    #should check if path is being read in correctly
+    image_strategy = RGBImageStrategy(
+    image_path="tests/segmentation_utils_tests/test_strategies",
+    image_size=(224, 224),
+    image_resample=Image.Resampling.NEAREST,
+)
+
+    batch_size = 2
+    dataset_index = 0
+    image_strategy.read_batch(batch_size, dataset_index)
+
+def test_read_batch_returns_nparray() -> None:
+    #checking if the returned value is a numpy array
+
+    image_strategy = RGBImageStrategy(
+    image_path="tests/segmentation_utils_tests/test_strategies",
+    image_size=(224, 224),
+    image_resample=Image.Resampling.NEAREST,
+)
+
+    result = image_strategy.read_batch(batch_size=2, dataset_index=0)
+    assert isinstance(result, np.ndarray) 
+
+def test_get_dataset_size() -> None:
+    #checking if the calculation is done correctly
+
+    image_strategy = RGBImageStrategy(
+    image_path="tests/segmentation_utils_tests/test_strategies",
+    image_size=(224, 224),
+    image_resample=Image.Resampling.NEAREST,
+)
+    dataset = 100 #if there are 100 images in the specified path
+    mini_batch = 32 #and we want 32 images in each batch
+    expected_value = dataset / mini_batch #number of sets of images we expect
+    
+    dataset_size = image_strategy.get_dataset_size(mini_batch)
+    assert dataset_size == expected_value
+
+
+
+
