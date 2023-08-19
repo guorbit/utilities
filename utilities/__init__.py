da5400a57cf839fef0c416e1f58c9aff6b919784
Sajtospoga01 <abodrogai@gmail.com>
2023-08-19 15:38:37 +0100

    fixes linting problems, lowers linting fail to 9.7

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -7,15 +7,16 @@ def get_version_from_pyproject():
     try:
         # Determine the path to pyproject.toml relative to this file
         base_dir = os.path.dirname(os.path.abspath(__file__))
-        pyproject_path = os.path.join(base_dir, '..', 'pyproject.toml')
-        
+        pyproject_path = os.path.join(base_dir, "..", "pyproject.toml")
+
         # Load the pyproject.toml file
         pyproject_data = toml.load(pyproject_path)
-        
+
         # Extract the version from the project section
         version = pyproject_data["project"]["version"]
-    except Exception as e:
+    except FileNotFoundError:
         version = "unknown"
     return version
+
 
 __version__ = get_version_from_pyproject()
