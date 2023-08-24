047789adcd167bf797a55197cfb6a7d6b84f36c8
Sajtospoga01 <abodrogai@gmail.com>
2023-08-24 15:59:09 +0100

    adds rasterio strategy test, with functional mock class.

--- tests/segmentation_utils_tests.py/test_strategies.py
+++ tests/segmentation_utils_tests.py/test_strategies.py
@@ -1,21 +1,27 @@
 import os
+
 import numpy as np
-from PIL import Image
 import rasterio
+from PIL import Image
 from pytest import MonkeyPatch
 
-from utilities.segmentation_utils.reading_strategies import RGBImageStrategy
+from utilities.segmentation_utils.reading_strategies import (
+    HyperspectralImageStrategy, MockRasterio, RGBImageStrategy)
 
 
 def test_read_batch_image_path() -> None:
-    #checking if the file is being opened and read correctly
+    # checking if the file is being opened and read correctly
     patch = MonkeyPatch()
 
     mock_filenames = ["a", "b", "c"]
 
     patch.setattr(os, "listdir", lambda x: mock_filenames)
 
-    patch.setattr(Image, "open", lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)))
+    patch.setattr(
+        Image,
+        "open",
+        lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)),
+    )
 
     image_strategy = RGBImageStrategy(
         image_path="tests/segmentation_utils_tests/test_strategies",
@@ -37,16 +43,20 @@ def test_read_batch_returns_nparray() -> None:
 
     patch.setattr(os, "listdir", lambda x: ["a", "b", "c"])
 
-    patch.setattr(Image, "open", lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)))
-    
+    patch.setattr(
+        Image,
+        "open",
+        lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)),
+    )
+
     image_strategy = RGBImageStrategy(
         image_path="tests/segmentation_utils_tests/test_strategies",
         image_size=(224, 224),
         image_resample=Image.Resampling.NEAREST,
     )
 
-    batch_size=2
-    dataset_index=0
+    batch_size = 2
+    dataset_index = 0
 
     result = image_strategy.read_batch(batch_size, dataset_index)
     assert isinstance(result, np.ndarray)
@@ -63,7 +73,12 @@ def test_get_dataset_size() -> None:
 
     patch.setattr(os, "listdir", lambda x: mock_filenames)
 
-    patch.setattr(Image, "open", lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)))
+    #! not needed as you arent reading any image in this function
+    patch.setattr(
+        Image,
+        "open",
+        lambda _: Image.fromarray(np.ones((224, 224, 3)).astype(np.uint8)),
+    )
 
     image_strategy = RGBImageStrategy(
         image_path="tests/segmentation_utils_tests/test_strategies",
@@ -72,50 +87,33 @@ def test_get_dataset_size() -> None:
     )
     dataset = len(mock_filenames)  # number of images in the specified path
     mini_batch = 2  # number of images we want in each batch
-    expected_value = int (np.floor(dataset / float(mini_batch)))  # number of sets of images we expect
+    expected_value = int(
+        np.floor(dataset / float(mini_batch))
+    )  # number of sets of images we expect
 
     dataset_size = image_strategy.get_dataset_size(mini_batch)
     assert dataset_size == expected_value
     patch.undo()
     patch.undo()
 
-class MockRasterio():
-    
-    def __init__(self):
-        self.shape = (224, 224) #dimensions for raster data 
-        self.dtypes = ['int32'] #data type of raster data that would be returned by .open()
-        #a list containing a string representing a data type
-        #32 bit int data type
-
-    def read(self, *args, **kwargs):
-        return np.zeros(self.shape, self.dtypes[0])
-    
-    #these functions are invoked when a 'with' statement is executed
-    def __enter__(self):
-        #called at the beginning of a 'with' block
-        return self #returns instance of MockRasterio class itself
-    
-    def __exit__(self, type, value, traceback):
-        #called at the end of a 'with' block
-        pass
 
 def test_hyperspectral_open():
-        patch = MonkeyPatch()
-        mock_filenames = ["a", "b", "c"]
-        patch.setattr(os, "listdir", lambda x: mock_filenames)
+    patch = MonkeyPatch()
+    mock_filenames = ["a", "b", "c"]
+    patch.setattr(os, "listdir", lambda x: mock_filenames)
 
-        def mock_open(*args, **kwargs): #local function to the test
-            #defines behaviour of mock object that replaces rasterio.open()
-            return MockRasterio()
-        
-        patch.setattr(rasterio, "open", mock_open)
-        image_path = "tests/segmentation_utils_tests/test_strategies"
-        dataset_list = []
-
-        for filename in mock_filenames:
-            file_path = os.path.join(image_path, filename)
-            dataset = rasterio.open(file_path)
-            dataset_list.append(dataset)
+    image_path = "tests/segmentation_utils_tests/test_strategies"
     
-            assert dataset.shape == (224, 224)
-            assert np.array_equal (dataset.read(), np.zeros((224, 224), dtype='int32'))
+    mock_data = {
+        "n": 3,
+        "size": (224, 224),
+        "bands": 3,
+        "dtypes": ["uint8"],
+    }
+    strategy = HyperspectralImageStrategy(
+        image_path, (224, 224), package=MockRasterio(**mock_data)
+    )
+
+    read_images = strategy.read_batch(2, 0)
+
+    assert read_images.shape == (2, 224, 224, 3)
--- utilities/segmentation_utils/reading_strategies.py
+++ utilities/segmentation_utils/reading_strategies.py
@@ -1,35 +1,63 @@
 import os
-from typing import Protocol
+from types import ModuleType
+from typing import Protocol, Type, Union
 
 import numpy as np
 import rasterio
 from PIL import Image
 
 
-class IReader(Protocol):
+class MockRasterio:
+    def __init__(self, n , size, bands, dtypes):
+        self.n = n
+        self.size = size
+        self.bands = bands
+        self.dtypes = dtypes
+ 
+    def open(self, *args, **kwargs):
+        return self
 
-    def read_batch(self, start:int, end: int) -> None:
+    @property
+    def count(self) -> int:
+        return self.bands
+
+    def read(self, *args, **kwargs):
+        return np.zeros((self.bands,self.size[0],self.size[1]), self.dtypes[0])
+
+    # these functions are invoked when a 'with' statement is executed
+    def __enter__(self):
+        # called at the beginning of a 'with' block
+        return self  # returns instance of MockRasterio class itself
+
+    def __exit__(self, type, value, traceback):
+        # called at the end of a 'with' block
+        pass
+
+
+class IReader(Protocol):
+    def read_batch(self, start: int, end: int) -> None:
         ...
-    
+
     def get_dataset_size(self) -> None:
         ...
 
-class RGBImageStrategy:
 
+class RGBImageStrategy:
     def __init__(
         self,
         image_path: str,
         image_size: tuple[int, int],
-        image_resample = Image.Resampling.NEAREST,
+        image_resample=Image.Resampling.NEAREST,
     ):
         self.image_path = image_path
-        self.image_filenames = np.array(sorted(os.listdir(self.image_path))) #!update: added variable to initialiser
+        self.image_filenames = np.array(
+            sorted(os.listdir(self.image_path))
+        )  #!update: added variable to initialiser
         self.image_size = image_size
         self.image_resample = image_resample
-        
 
     def read_batch(self, batch_size, dataset_index) -> np.ndarray:
-        #read images with PIL
+        # read images with PIL
 
         for i in range(batch_size):
             image_index = i + dataset_index
@@ -43,44 +71,49 @@ class RGBImageStrategy:
     def get_dataset_size(self, mini_batch) -> int:
         dataset_size = int(np.floor(len(self.image_filenames) / float(mini_batch)))
         return dataset_size
-    
+
+
 class HyperspectralImageStrategy:
-    #read images with rasterio
+    # read images with rasterio
     def __init__(
         self,
-        image_path:str,
-        image_resize:tuple[int,int],
-        image_resample = Image.Resampling.NEAREST,
-        package = rasterio
+        image_path: str,
+        image_resize: tuple[int, int],
+        image_resample=Image.Resampling.NEAREST,
+        package: Union[MockRasterio, ModuleType] = rasterio,
     ):
         self.image_path = image_path
         self.image_filenames = np.array(sorted(os.listdir(self.image_path)))
         self.image_resize = image_resize
         self.image_resample = image_resample
         self.package = package
-        #gets the number of bands for the dataset
-        self.bands = package.open(os.path.join(self.image_path, self.image_filenames[0])).count
-  
-    def read_batch(self, batch_size, dataset_index) -> np.ndarray:
-        #read images with rasterio
-        batch_filenames = self.image_filenames[dataset_index:dataset_index + batch_size]
+        # gets the number of bands for the dataset
+        self.bands = package.open(
+            os.path.join(self.image_path, self.image_filenames[0])
+        ).count
+        print("-----------My very cool bands--------: ",self.bands)
 
-        #defines the array that will contain the images
-        images = np.zeros((batch_size, self.bands, self.image_resize[0], self.image_resize[1]))
-        for i,filename in enumerate(batch_filenames):
-            with self.package.open(
-                os.path.join(self.image_path, filename)
-                ) as dataset:
-                 #.read() returns a numpy array that contains the raster cell values in your file.
+    def read_batch(self, batch_size:int, dataset_index:int) -> np.ndarray:
+        # read images with rasterio
+        batch_filenames = self.image_filenames[
+            dataset_index : dataset_index + batch_size
+        ]
+
+        # defines the array that will contain the images
+        images = np.zeros(
+            (batch_size, self.bands, self.image_resize[0], self.image_resize[1])
+        )
+        for i, filename in enumerate(batch_filenames):
+            with self.package.open(os.path.join(self.image_path, filename)) as dataset:
+                # .read() returns a numpy array that contains the raster cell values in your file.
                 image = dataset.read()
-            images[i,:,:,:] = np.resize(image,self.image_resize)
+            images[i, :, :, :] = np.resize(image, self.image_resize)
 
-        #ensures channel-last orientation for the reader
-        np.moveaxis(images,1,3)
-   
+        # ensures channel-last orientation for the reader
+        images = np.moveaxis(images, 1, 3)
+
         return np.array(images)
-    
+
     def get_dataset_size(self, mini_batch) -> int:
         dataset_size = int(np.floor(len(self.image_filenames) / float(mini_batch)))
         return dataset_size
-    
